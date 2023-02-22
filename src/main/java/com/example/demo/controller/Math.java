1f62bfc0d17f57b621a9c12a5769a697f68dd910
tet1917 <tetsu0224.soccer@icloud.com>
2023-02-23 02:57:17 +0900

    no message

--- src/main/java/com/example/demo/controller/Math.java
+++ src/main/java/com/example/demo/controller/Math.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class Math {
 	 @GetMapping("/index")

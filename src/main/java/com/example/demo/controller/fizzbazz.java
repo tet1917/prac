a49da313f18456035b60d3d72a200b64e35b007c
tet1917 <tetsu0224.soccer@icloud.com>
2023-02-26 20:12:39 +0900

    no message

--- src/main/java/com/example/demo/controller/fizzbazz.java
+++ src/main/java/com/example/demo/controller/fizzbazz.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
 public class fizzbazz {
-
+    public static void main(String[] args) {
+        // for文と条件分岐を使った書き方
+        for ( int i = 1; i <= 30; i++ ) {
+            if ( i % 3 == 0 && i % 5 == 0 ) System.out.print("FizzBuzz");
+            else if ( i % 3 == 0 ) System.out.print("Fizz");
+            else if ( i % 5 == 0 ) System.out.print("Buzz");
+            else System.out.print(i);
+        }
+    }
 }
